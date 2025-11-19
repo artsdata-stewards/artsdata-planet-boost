cf7876431fd0f92844f01e862df98680a9ada5e3
dev <dev.aravind@culturecreates.com>
2025-11-19 13:54:37 +0530

    test(workflow): checking parallel job count

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+gem "sparql-client", "~> 3.3"
+gem "json"
+gem "rexml"         # Required by sparql-client
+gem "nokogiri"      # Required by sparql-client
---
+++ src/fetch_organizations.rb
@@ -0,0 +1,45 @@
+# gem install sparql
+# http://www.rubydoc.info/github/ruby-rdf/sparql/frames
+
+require 'sparql/client'
+require 'json'
+require 'uri'
+
+endpoint = "https://query.wikidata.org/sparql"
+sparql = <<'SPARQL'.chop
+SELECT ?org ?artsdataID ?url WHERE {
+  ?org wdt:P31/wdt:P279* wd:Q7168296 .
+  ?org wdt:P7627 ?artsdataID .
+  ?org wdt:P856 ?url .
+}
+SPARQL
+
+client = SPARQL::Client.new(
+  endpoint,
+  method: :get,
+  headers: { 'User-Agent' => 'artsdata-crawler/3.3' }
+)
+
+rows = client.query(sparql)
+
+def artifact_from_url(url)
+  host = URI.parse(url).host rescue nil
+  return nil unless host
+  host = host.sub(/^www\./, '')
+  host.gsub('.', '-')
+end
+
+data = rows.map do |row|
+  url = row[:url].to_s
+
+  {
+    "org" => row[:org].to_s,
+    "artsdataID" => row[:artsdataID].to_s,
+    "url" => url,
+    "artifact" => artifact_from_url(url)
+  }
+end
+
+File.write("wikidata_orgs.json", JSON.pretty_generate(data))
+
+puts "Saved #{data.size} rows to wikidata_orgs.json"
