23f81fa9900f5f9e68a5504c58740c47d7c71fd6
dev <dev.aravind@culturecreates.com>
2025-11-27 15:44:48 +0530

    enhancement(skip): added skippable websites

---
+++ src/config.rb
@@ -0,0 +1,15 @@
+module Config
+  WIKIDATA_CONFIG = {
+    wikidata_endpoint: "https://query.wikidata.org/sparql",
+    wikidata_sparql: "sparql/wikidata_query.sparql",
+    artsdata_sparql: "sparql/artsdata_already_existing.sparql",
+    artsdata_endpoint: "https://staging.db.artsdata.ca/repositories/artsdata",
+    do_not_load_websites: [
+      "https://www.ville.lamalbaie.qc.ca/"
+    ]
+  }.freeze
+
+  GENERAL_CONFIG = {
+    user_agent: 'artsdata-crawler/3.3'
+  }.freeze
+end
--- src/fetch_organizations.rb
+++ src/fetch_organizations.rb
@@ -5,24 +5,25 @@ require 'sparql/client'
 require 'json'
 require 'uri'
 require 'securerandom'
+require_relative 'config'
 
-endpoint = "https://query.wikidata.org/sparql"
-sparql = <<'SPARQL'.chop
-SELECT ?org ?artsdataID ?url ?name WHERE {
-  ?org wdt:P31/wdt:P279* wd:Q7168296 .
-  ?org wdt:P7627 ?artsdataID .
-  ?org wdt:P856 ?url .
-  ?org rdfs:label ?name .
-}
-SPARQL
+wikidata_endpoint = Config::WIKIDATA_CONFIG[:wikidata_endpoint]
+wikidata_sparql = File.read(Config::WIKIDATA_CONFIG[:wikidata_sparql])
 
-client = SPARQL::Client.new(
-  endpoint,
+wikidata_client = SPARQL::Client.new(
+  wikidata_endpoint,
   method: :get,
-  headers: { 'User-Agent' => 'artsdata-crawler/3.3' }
+  headers: { 'User-Agent' => Config::GENERAL_CONFIG[:user_agent] }
 )
 
-rows = client.query(sparql)
+rows = wikidata_client.query(wikidata_sparql)
+
+uri = URI(Config::WIKIDATA_CONFIG[:artsdata_endpoint])
+artsdata_sparql = File.read(Config::WIKIDATA_CONFIG[:artsdata_sparql])
+response = Net::HTTP.post_form(uri, { "query" => artsdata_sparql })
+already_existing = response.body.lines[1..].map(&:chomp).to_set
+
+do_not_load = Config::WIKIDATA_CONFIG[:do_not_load_websites].to_set
 
 def artifact_from_url(url)
   host = URI.parse(url).host rescue nil
@@ -35,7 +36,7 @@ date = Time.now.strftime("%Y-%m-%d-%H:%M")
 uuid = SecureRandom.uuid
 data = rows.map do |row|
   url = row[:url].to_s
-  {
+  meta = {
     "file_name" => "metadata_wikidata_spider.jsonld",
     "url" => url,
     "artifact" => artifact_from_url(url),
@@ -45,6 +46,18 @@ data = rows.map do |row|
     "datafeed_title" => "Collection of Wikidata presenter websites",
     "metadata_artifact" => "wikidata-spider-crawl-metadata"
   }
+  if already_existing.include?(url)
+    meta['crawl_name'] = "Website skipped",
+    meta['crawl_description'] = "Skipped crawl because website is already loaded by another activity."
+    meta['skip_crawl'] = true
+  end
+
+  if do_not_load.include?(url)
+    meta['crawl_name'] = "Website flagged to not load",
+    meta['crawl_description'] = "This website has been flagged to not be loaded."
+    meta['skip_crawl'] = true
+  end
+  meta
 end
 data = data.uniq { |d| d["url"] }
 data = data.uniq { |d| d["same_as"] }
