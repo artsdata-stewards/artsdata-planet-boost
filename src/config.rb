c7c96e6955533e23c64d2e871de55f98e924d6c6
dev-aravind <132546883+dev-aravind@users.noreply.github.com>
2025-11-28 17:19:48 +0530

    Update config.rb

--- src/config.rb
+++ src/config.rb
@@ -3,7 +3,7 @@ module Config
     wikidata_endpoint: "https://query.wikidata.org/sparql",
     wikidata_sparql: "sparql/wikidata_query.sparql",
     artsdata_sparql: "sparql/artsdata_already_existing.sparql",
-    artsdata_endpoint: "https://staging.db.artsdata.ca/repositories/artsdata",
+    artsdata_endpoint: "https://db.artsdata.ca/repositories/artsdata",
     do_not_load_websites: [
       "https://www.ville.lamalbaie.qc.ca/"
     ]
