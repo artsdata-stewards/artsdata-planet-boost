ef59d23eb90c508ea1ead6c4789858ddc0d9ef37
devnandan-cc <devnandan@culturecreates.com>
2026-03-31 14:16:32 +0530

    enhancement : add outside canada websites to the config in the repo so it DOES NOT crawl

--- src/config.rb
+++ src/config.rb
@@ -3,7 +3,14 @@ module Config
   GENERAL_CONFIG = {
     user_agent: 'artsdata-crawler/3.3',
     do_not_load_websites: [
-      "https://www.ville.lamalbaie.qc.ca"
+      "https://www.ville.lamalbaie.qc.ca",
+      "https://www.sfo.org.uk/",
+      "https://www.bushtheatre.co.uk/",
+      "http://www.fretwork.co.uk/",
+      "https://bonbonvodou.fr/",
+      "https://www.thetallisscholars.co.uk/",
+      "https://www.cie-koubi.fr/",
+      "https://www.ballet.org.uk/"
     ]
   }.freeze
 end
